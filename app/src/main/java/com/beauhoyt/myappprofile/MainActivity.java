70596f24ca37680516c15447011e5244eb90ff19
Beau Hoyt <beau.g.hoyt@gmail.com>
2015-06-15 23:22:00 -0700

    Make some code improvements along with some clean up
    - Add colors.xml (move colors into this file)
    - Move static dimentions to dimens.xml
    - Move static strings in java classes into strings.xml for easy translation
      later
    - Set both layouts to have scrolling views
    - Update both layouts to use variables from colors.xml, dimentions.xml, and
      strings.xml
    - Refactor code in MainActivity.java class to use a clickOnButton() method that
      is more dynamic instead of having the same code over and over again
    - Refactor similar code for creating toasts in to a helper method
      longTextToast()

--- app/src/main/java/com/beauhoyt/myappprofile/MainActivity.java
+++ app/src/main/java/com/beauhoyt/myappprofile/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import android.content.Context;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -39,57 +40,15 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void launchSpotifyStreamer(View view) {
+    public void longTextToast(String s) {
         Context context = getApplicationContext();
-        CharSequence text = "This button will launch spotify streamer!";
         int duration = Toast.LENGTH_LONG;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        Toast.makeText(context, s, duration).show();
     }
 
-    public void launchScoresApp(View view) {
-        Context context = getApplicationContext();
-        CharSequence text = "This button will launch scores app!";
-        int duration = Toast.LENGTH_LONG;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-    }
-
-    public void launchLibraryApp(View view) {
-        Context context = getApplicationContext();
-        CharSequence text = "This button will launch library app!";
-        int duration = Toast.LENGTH_LONG;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-    }
-
-    public void launchBuildItBigger(View view) {
-        Context context = getApplicationContext();
-        CharSequence text = "This button will launch build it bigger!";
-        int duration = Toast.LENGTH_LONG;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-    }
-
-    public void launchXyzReader(View view) {
-        Context context = getApplicationContext();
-        CharSequence text = "This button will launch xyz reader!";
-        int duration = Toast.LENGTH_LONG;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-    }
-
-    public void launchCapstone(View view) {
-        Context context = getApplicationContext();
-        CharSequence text = "This button will launch my capstone app!";
-        int duration = Toast.LENGTH_LONG;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+    public void clickOnButton(View view) {
+        Button btn = (Button) view;
+        String toastFormat = getResources().getString(R.string.click_on_button_toast);
+        longTextToast(String.format(toastFormat, btn.getText().toString().toLowerCase()));
     }
 }
